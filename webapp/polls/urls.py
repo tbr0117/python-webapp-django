a3adf503dcd49051a3ca62a213e51f74fc8d9a18
tbr0117 <bhargav.tanguturi@pivoxlabs.com>
2021-12-24 01:47:06 +0530

    added more views

--- webapp/polls/urls.py
+++ webapp/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('<int:QuestionId>/', views.detail, name="detail"),
+    path("<int:QuestionId>/result", views.result, name="result"),
+    path("<int:QuestionId>/vote", views.vote, name="vote"),
 ]
--- webapp/polls/views.py
+++ webapp/polls/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 
+# Create your views here.
 def index(request):
     return HttpResponse('Hello World, this is my world ')
 
-# Create your views here.
+def detail(request, QuestionId):
+    return HttpResponse("You're looking at question %s" % QuestionId)
+
+def result(request, QuestionId):
+    response = "You're looking at the result of question %s"
+    return HttpResponse(response % QuestionId)
+
+def vote(request, QuestionId):
+    return HttpResponse("You're voting on question %s." % QuestionId)
+    
+
